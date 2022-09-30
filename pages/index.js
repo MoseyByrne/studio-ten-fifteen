15b356bfbaeb2bb7f5c41035ab3bc3a2617b0e06
MoseyByrne <mosey.b.byrne@gmail.com>
2022-09-30 12:17:07 -0700

    WIP product image and price not showing on webpage

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = ({ products, bannerData }) => {
       </div>
 
       <div className="products-container">
-      {products && (products.map(product => <Product key={product.id} product={product} />))}
+      {products && (products.map(product => <Product key={product._id} product={product} />))}
     </div>
 
 
