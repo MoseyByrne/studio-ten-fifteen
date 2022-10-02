7d0acc8d2abe9350c4159ba9caa5a20776b210d6
MoseyByrne <mosey.b.byrne@gmail.com>
2022-10-02 12:03:00 +0100

    WIP add state to app

--- context/StateContext.js
+++ context/StateContext.js
@@ -13,6 +13,8 @@ export const StateContext = ({ children }) => {
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find((item) => item._id === product._id);
 
+    Sun Oct 02 11:10 2022
+
     setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
 
