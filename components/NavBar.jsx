4a221137535c475c3a03d9f76bfb4ed148a619ba
MoseyByrne <mosey.b.byrne@gmail.com>
2022-10-02 17:14:30 -0700

    add Cart

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai';
 
+import { Cart } from './';
+
 const NavBar = () => {
   return (
     <div className="navbar-container">
@@ -14,6 +16,8 @@ const NavBar = () => {
         <AiOutlineShopping />
         <span className="cart-item-qty">1</span>
       </button>
+
+      <Cart />
     </div>
   )
 }
