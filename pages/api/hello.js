56ce94a65aa1b0b47c7b97b6f4ba109fa553a97a
MoseyByrne <mosey.b.byrne@gmail.com>
2022-10-04 15:59:18 -0700

    start implementing stripe

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// this api folder essentially serves the entire back end of our application. the part that's written inside of the api is going to be our server, and that's not going to be rendered on the front end, so inside of a next.js app we don't have a need for a special node and express server. we can do everything inside of this api folder.
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
